245918fd1fd39cd982ab781c0d0bca6ef2573892
Alexandra <angyeemarin.dev@gmail.com>
2025-04-09 20:33:56 -0400

    chore: add new paths

--- constants/path.py
+++ constants/path.py
@@ -5,4 +5,7 @@ class PathConstant:
     """All paths neccessary to the system."""
 
     FILEPATH_LOGS = f"{path.realpath("./")}/system.log"
-    DATA_BORDER =   f"{path.realpath("./")}/data/Borde"
+    SCAN_DATA_BORDER =   f"{path.realpath("./")}/data/SCAN/Borde"
+    SCAN_DATA_BRAS =   f"{path.realpath("./")}/data/SCAN/Bras"
+    SCAN_DATA_CACHING =   f"{path.realpath("./")}/data/SCAN/Caching"
+    SCAN_DATA_RAI =   f"{path.realpath("./")}/data/SCAN/Rai"
