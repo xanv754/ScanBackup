a71e047f2a824bc40649b47be3dc3aeab078a68d
Angxandralol <angyeemarin.dev@gmail.com>
2025-04-24 11:11:13 -0400

    chore: adjusted init and add new method

--- utils/log.py
+++ utils/log.py
@@ -1,6 +1,6 @@
 import rich
 import art
-from typing import List
+from typing import Set, List
 from datetime import datetime
 from rich.text import Text
 from rich.console import Console, Group
@@ -14,8 +14,9 @@ class LogHandler:
     """Handler to realize all operation about log system."""
 
     __instance:  "LogHandler | None" = None
-    __stdout: List[Text] = [Text(f"{TITLE_ART}", "dark_slate_gray2")]
+    __register: Set = set()
     __console: Console
+    __started: bool = False
 
     def __new__(cls):
         if not cls.__instance:
@@ -25,21 +26,13 @@ class LogHandler:
     def __init__(self):
         try:
             if not hasattr(self, "__initialized"):
-                self.__initialized = True
                 self.__console = Console()
+                self.__initialized = True
         except Exception as e:
             rich.print(f"Log Error - {e}")
 
-    @classmethod
-    def log(cls, message: str, path: str | None = None, err: bool = False, warn: bool = False, info: bool = False, cprint: bool = True) -> None:
-        instance = cls.__new__(cls)
-        instance.__init__()
-        message = message.strip()
-        if cprint:
-            instance.cprint(message, path=path, err=err, warn=warn, info=info)
-        instance.save(message, path=path, err=err, warn=warn, info=info)
 
-    def cprint(self, message: str, path: str | None = None, err: bool = False, warn: bool = False, info: bool = False) -> None:
+    def __cprint(self, message: str, path: str | None = None, err: bool = False, warn: bool = False, info: bool = False) -> None:
         """Print message in terminal.
 
         Parameters
@@ -55,70 +48,79 @@ class LogHandler:
         info : bool
             If the message is an info. Default false.
         """
-        if path:
+        date = datetime.now().strftime("%Y-%m-%d %H:%M")
+        date_complete = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        if err and path:
             path = path.split(f"/{AboutConstant.TITLE_PROJECT.value}/")[1]
-        date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        if err:
-            if path:
-                message_print = Text.assemble(
-                    (date, "orange3"),
-                    (f" ({path})", "deep_sky_blue1"),
-                    (f" ERROR", "red3"),
-                    (f" {message}")
-                )
-            else:
-                message_print = Text.assemble(
-                    (date, "orange3"),
-                    (f" ERROR", "red3"),
-                    (f" {message}")
-                )
+            message_print = Text.assemble(
+                (date_complete, "orange3"),
+                (f" ({path})", "deep_sky_blue1"),
+                (f" ERROR", "red3"),
+                (f" {message}")
+            )
+            message_str = f"{date} ({path}) ERROR {message}"
+        elif err:
+            message_print = Text.assemble(
+                (date_complete, "orange3"),
+                (f" ERROR", "red3"),
+                (f" {message}")
+            )
+            message_str = f"{date} ERROR {message}"
+        elif warn and path:
+            path = path.split(f"/{AboutConstant.TITLE_PROJECT.value}/")[1]
+            message_print = Text.assemble(
+                (date_complete, "orange3"),
+                (f" ({path})", "deep_sky_blue1"),
+                (f" WARNING", "gold1"),
+                (f" {message}")
+            )
+            message_str = f"{date} ({path}) WARNING {message}"
         elif warn:
-            if path:
-                message_print = Text.assemble(
-                    (date, "orange3"),
-                    (f" ({path})", "deep_sky_blue1"),
-                    (f" WARNING", "gold1"),
-                    (f" {message}")
-                )
-            else:
-                message_print = Text.assemble(
-                    (date, "orange3"),
-                    (f" WARNING", "gold1"),
-                    (f" {message}")
-                )
+            message_print = Text.assemble(
+                (date_complete, "orange3"),
+                (f" WARNING", "gold1"),
+                (f" {message}")
+            )
+            message_str = f"{date} WARNING {message}"
+        elif info and path:
+            path = path.split(f"/{AboutConstant.TITLE_PROJECT.value}/")[1]
+            message_print = Text.assemble(
+                (date_complete, "orange3"),
+                (f" ({path})", "deep_sky_blue1"),
+                (f" INFO", "chartreuse2"),
+                (f" {message}")
+            )
+            message_str = f"{date} ({path}) INFO {message}"
         elif info:
-            if path:
-                message_print = Text.assemble(
-                    (date, "orange3"),
-                    (f" ({path})", "deep_sky_blue1"),
-                    (f" INFO", "chartreuse2"),
-                    (f" {message}")
-                )
-            else:
-                message_print = Text.assemble(
-                    (date, "orange3"),
-                    (f" INFO", "chartreuse2"),
-                    (f" {message}")
-                )
+            message_print = Text.assemble(
+                (date_complete, "orange3"),
+                (f" INFO", "chartreuse2"),
+                (f" {message}")
+            )
+            message_str = f"{date} INFO {message}"
+        elif not err and not warn and not info and path:
+            path = path.split(f"/{AboutConstant.TITLE_PROJECT.value}/")[1]
+            message_print = Text.assemble(
+                (date_complete, "orange3"),
+                (f" ({path})", "deep_sky_blue1"),
+                (f" {message}")
+            )
+            message_str = f"{date} ({path}) {message}"
         else:
-            if path:
-                message_print = Text.assemble(
-                    (date, "orange3"),
-                    (f" ({path})", "deep_sky_blue1"),
-                    (f" {message}")
-                )
-            else:
-                message_print = Text.assemble(
-                    (date, "orange3"),
-                    (f" {message}")
-                )
-        self.__stdout.append(message_print)
-        group = Group(*self.__stdout)
-        panel = Panel(group, title="Sistema CGPRD", style="dark_slate_gray2")
-        self.__console.clear()
-        self.__console.print(panel)
+            message_print = Text.assemble(
+                (date_complete, "orange3"),
+                (f" ({path})", "deep_sky_blue1"),
+                (f" {message}")
+            )
+            message_str = f"{date} {message}"
+        if not message_str in self.__register:
+            self.__register.add(message_str)
+            if not self.__started:
+                self.__console.print(Text(f"{TITLE_ART}", "dark_slate_gray2"))
+                self.__started = True
+            self.__console.print(message_print)
 
-    def save(self, message: str, path: str | None = None, err: bool = False, warn: bool = False, info: bool = False) -> None:
+    def __save(self, message: str, path: str | None = None, err: bool = False, warn: bool = False, info: bool = False) -> None:
         """Save a message on the log file of system. Also print message in terminal.
 
         Parameters
@@ -160,8 +162,25 @@ class LogHandler:
         with open(PathConstant.FILEPATH_LOGS, "a") as file:
             file.write(f"{message}\n")
 
+    def export(self, message: str, path: str | None = None, err: bool = False, warn: bool = False, info: bool = False, cprint: bool = True) -> None:
+        message = message.strip()
+        if cprint:
+            self.__cprint(message, path=path, err=err, warn=warn, info=info)
+        self.__save(message, path=path, err=err, warn=warn, info=info)
+
+    def print(self, message: str, path: str | None = None) -> None:
+        messages: List[str] = []
+        if path:
+            path = path.split(f"/{AboutConstant.TITLE_PROJECT.value}/")[1]
+            messages.append(f"Path print: {path}\n")
+        messages.append(message)
+        group = Group(*messages)
+        panel = Panel(group, title="Debug", style="dark_slate_gray2", title_align="left")
+        self.__console.print(panel)
 
 if __name__ == "__main__":
-    LogHandler.log("This is a test", path=__file__, err=True)
-    LogHandler.log("This is a test", path=__file__, warn=True)
-    LogHandler.log("This is a test", path=__file__, info=True)
+    log = LogHandler()
+    log.export("This is a test", path=__file__, err=True)
+    log.export("This is a test", path=__file__, warn=True)
+    log.print("This is a test", path=__file__)
+    log.export("This is a test", path=__file__, info=True)
