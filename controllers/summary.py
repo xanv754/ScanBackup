4fbb8daf8b96e87b9417824dd11ec12337574f24
xanv754 <angyeemarin.dev@gmail.com>
2025-05-14 13:42:04 -0400

    feat(process): summary diary

--- controllers/summary.py
+++ controllers/summary.py
@@ -17,20 +17,21 @@ class SummaryController:
         """
         traffic = TrafficHandler()
         df_data_borde = traffic.get_traffic_layer_by_days_before(layer_type=LayerType.BORDE, day_before=1)
-        df_summary_borde = calculate(df_data_borde)
-        df_data_bras = traffic.get_traffic_layer_by_days_before(layer_type=LayerType.BRAS, day_before=1)
-        df_summary_bras = calculate(df_data_bras)
-        df_data_caching = traffic.get_traffic_layer_by_days_before(layer_type=LayerType.CACHING, day_before=1)
-        df_summary_caching = calculate(df_data_caching)
-        df_data_rai = traffic.get_traffic_layer_by_days_before(layer_type=LayerType.RAI, day_before=1)
-        df_summary_rai = calculate(df_data_rai)
-        data = {
-            LayerType.BORDE: df_summary_borde,
-            LayerType.BRAS: df_summary_bras,
-            LayerType.CACHING: df_summary_caching,
-            LayerType.RAI: df_summary_rai
-        }
-        return data
+        print(df_data_borde)
+        # df_summary_borde = calculate(df_data_borde)
+        # df_data_bras = traffic.get_traffic_layer_by_days_before(layer_type=LayerType.BRAS, day_before=1)
+        # df_summary_bras = calculate(df_data_bras)
+        # df_data_caching = traffic.get_traffic_layer_by_days_before(layer_type=LayerType.CACHING, day_before=1)
+        # df_summary_caching = calculate(df_data_caching)
+        # df_data_rai = traffic.get_traffic_layer_by_days_before(layer_type=LayerType.RAI, day_before=1)
+        # df_summary_rai = calculate(df_data_rai)
+        # data = {
+        #     LayerType.BORDE: df_summary_borde,
+        #     LayerType.BRAS: df_summary_bras,
+        #     LayerType.CACHING: df_summary_caching,
+        #     LayerType.RAI: df_summary_rai
+        # }
+        # return data
 
 
 if __name__ == "__main__":
