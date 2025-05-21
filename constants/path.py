60271189d8cd9a9cf3e6fc6171d09ac8da7710ec
xanv574 <angyeemarin.dev@gmail.com>
2025-05-21 08:55:21 -0400

    fix: constants of data path

--- constants/path.py
+++ constants/path.py
@@ -4,7 +4,7 @@ from os import path
 class DataPath:
     """All paths of the data system."""
 
-    SCAN_DATA_BORDER =   f"{path.realpath("./")}/data/SCAN/Borde"
-    SCAN_DATA_BRAS =   f"{path.realpath("./")}/data/SCAN/Bras"
-    SCAN_DATA_CACHING =   f"{path.realpath("./")}/data/SCAN/Caching"
-    SCAN_DATA_RAI =   f"{path.realpath("./")}/data/SCAN/RAI"
+    SCAN_DATA_BORDER =   f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/Borde"
+    SCAN_DATA_BRAS =   f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/Bras"
+    SCAN_DATA_CACHING =   f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/Caching"
+    SCAN_DATA_RAI =   f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/RAI"
