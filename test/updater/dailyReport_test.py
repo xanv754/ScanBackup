936dcbf7c6924f7d3b9ce8206d3e66fd6fa7f689
xanv754 <angyeemarin.dev@gmail.com>
2025-06-05 14:49:22 -0400

    test(process): dialy report updater unit test

---
+++ test/updater/dailyReport_test.py
@@ -0,0 +1,15 @@
+import unittest 
+
+
+class TestDailyReportUpdater(unittest.TestCase):
+    def test_get_data(self):
+        """Test get all data from daily report files."""
+        pass
+
+    def test_load_data(self):
+        """Test load all data from daily report files."""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
