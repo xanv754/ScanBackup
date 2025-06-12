9ede6f53524a68022ec2b6adf7927de87284889b
xanv754 <angyeemarin.dev@gmail.com>
2025-06-12 13:11:52 -0400

    style: delete spaces

--- constants/path.py
+++ constants/path.py
@@ -4,8 +4,8 @@ from os import path
 class DataPath:
     """All paths of the data system."""
 
-    SCAN_DATA_BORDER =   f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/Borde"
-    SCAN_DATA_BRAS =   f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/Bras"
-    SCAN_DATA_CACHING =   f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/Caching"
-    SCAN_DATA_RAI =   f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/RAI"
-    SCAN_REPORT_DIALY =   f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/Reportes-Diarios"
+    SCAN_DATA_BORDER = f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/Borde"
+    SCAN_DATA_BRAS = f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/Bras"
+    SCAN_DATA_CACHING = f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/Caching"
+    SCAN_DATA_RAI = f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/RAI"
+    SCAN_REPORT_DIALY = f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/Reportes-Diarios"
