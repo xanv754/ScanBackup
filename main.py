13bbfaed7bbf89ebbd2103d5f9425a0be607b729
xanv754 <angyeemarin.dev@gmail.com>
2025-06-13 15:04:37 -0400

    styles: Spanish languages to CLI

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 import click
 from controllers.summary import SummaryController
-
+from utils.log import log
 
 @click.group()
 def cli():
     """CGPRD CLI.
     
-    SystemCGPRD is a system to manage the data of the CGPRD.
+    Un sistema para gestión y almacenamiento de los datos de los reportes de la 
+    Coordinación Gestión Producto Red de Datos.
     """
     pass
 
 
-@cli.command(help="Get a summary report of the current day's data.")
+@cli.command(help="Obtiene el reporte diario.")
 @click.option("--date", required=False, help="Date to get data. Format YYYY-MM-DD")
-def diary(date: str):
-    SummaryController.summary_diary_current()
+def diario(date: str):
+    log.info("Obteniendo resumen diario...")
+    status_operation = SummaryController.summary_diary_current()
+    if not status_operation: log.error("No se pudo obtener el resumen diario")
+    else: log.info("Resumen diario obtenido")
 
-@cli.command(help="Get a summary of the current fortnight's data.")
-def fortnight():
+@cli.command(help="Obtiene el reporte quincenal.")
+def quincenal():
     SummaryController.summary_fortnight_current()
 
-@cli.command(help="Get a summary of the current weekly's data.")
-def weekly():
+@cli.command(help="Obtiene el reporte semanal.")
+def semanal():
     SummaryController.summary_weekly_current()
 
-@cli.command(help="Obtain a summary of the current month's data.")
-def monthly():
+@cli.command(help="Obtiene el reporte mensual.")
+def mensual():
     SummaryController.summary_monthly_current()
 
 
