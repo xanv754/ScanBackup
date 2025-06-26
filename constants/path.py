b0bbb937c7d9379eaa6cb5cbe75aa735c04e445a
xanv574 <angyeemarin.dev@gmail.com>
2025-06-26 14:12:07 -0400

    feat: generate daily reports

--- constants/path.py
+++ constants/path.py
@@ -1,5 +1,22 @@
 from os import path
 
+class LayerNames:
+    """All layers names."""
+
+    BORDER = "Borde"
+    BRAS = "Bras"
+    CACHING = "Caching"
+    IP_BRAS = "IPBras"
+    RAI = "RAI"
+
+
+layers_BBIP_SCAN = [
+    LayerNames.BORDER,
+    LayerNames.BRAS,
+    LayerNames.CACHING,
+    LayerNames.RAI
+]
+
 
 class DataPath:
     """All paths of the data system."""
@@ -9,3 +26,11 @@ class DataPath:
     SCAN_DATA_CACHING = f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/Caching"
     SCAN_DATA_RAI = f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/RAI"
     SCAN_REPORT_DAILY = f"{path.abspath(__file__).split("/constants")[0]}/data/SCAN/Reportes-Diarios"
+
+
+paths_BBIP_SCAN = [
+    DataPath.SCAN_DATA_BORDER,
+    DataPath.SCAN_DATA_BRAS,
+    DataPath.SCAN_DATA_CACHING,
+    DataPath.SCAN_DATA_RAI
+]
