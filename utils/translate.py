2c9dfd007980cbcd753df5063cb4677993e2cfa3
Alexandra <angyeemarin.dev@gmail.com>
2025-07-01 19:11:24 -0400

    chore: new columns translation

--- utils/translate.py
+++ utils/translate.py
@@ -25,9 +25,9 @@ class Translate:
                 elif column == HeaderBBIP.TYPE_LAYER:
                     new_columns.append("Capa")
                 elif column == HeaderBBIP.IN_VALUE:
-                    new_columns.append("In Prom")
+                    new_columns.append("In")
                 elif column == HeaderBBIP.OUT_VALUE:
-                    new_columns.append("Out Prom")
+                    new_columns.append("Out")
                 elif column == HeaderBBIP.IN_MAX:
                     new_columns.append("In Max")
                 elif column == HeaderBBIP.OUT_MAX:
@@ -36,6 +36,10 @@ class Translate:
                     new_columns.append("In Max Prom")
                 elif column == HeaderDailyReport.OUT_MAX:
                     new_columns.append("Out Max Prom")
+                elif column == HeaderDailyReport.IN_PROM:
+                    new_columns.append("In Prom")
+                elif column == HeaderDailyReport.OUT_PROM:
+                    new_columns.append("Out Prom")
                 elif column == HeaderDailyReport.USE:
                     new_columns.append("Uso (%)")
                 elif column == HeaderIPBras.BRAS_NAME:
