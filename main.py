0c53dcf622ba1ce8205201acd14944afb0c19c01
xanv754 <angyeemarin.dev@gmail.com>
2025-07-02 10:47:52 -0400

    chore: add new option to report commands

--- main.py
+++ main.py
@@ -22,24 +22,26 @@ def diario(date: str):
     else: log.info("Resumen diario generado con éxito")
 
 @cli.command(help="Obtiene el reporte semanal.")
-@click.option("--literal", required=False, help="Obtiene el resumen semanal contando los 7 días hacia atrás.")
-def semanal():
+@click.option("--literal", is_flag=True, required=False, help="Obtiene el resumen semanal contando los 7 días hacia atrás.")
+def semanal(literal: bool = False):
     log.info("Obteniendo resumen semanal...")
-    status_operation =SummaryReportBBIP().summary_weekly()
+    status_operation = SummaryReportBBIP().summary_weekly(literal=literal)
     if not status_operation: log.error("No se pudo generar el resumen semanal")
     else: log.info("Resumen semanal generado con éxito")
 
 @cli.command(help="Obtiene el reporte quincenal.")
-def quincenal():
+@click.option("--literal", is_flag=True, required=False, help="Obtiene el resumen semanal contando los 15 días hacia atrás.")
+def quincenal(literal: bool = False):
     log.info("Obteniendo resumen quincenal...")
-    status_operation = SummaryReportBBIP().summary_fortnight()
+    status_operation = SummaryReportBBIP().summary_fortnight(literal=literal)
     if not status_operation: log.error("No se pudo generar el resumen quincenal")
     else: log.info("Resumen quincenal generado con éxito")
 
 @cli.command(help="Obtiene el reporte mensual.")
-def mensual():
+@click.option("--literal", is_flag=True, required=False, help="Obtiene el resumen semanal contando los 30 días hacia atrás.")
+def mensual(literal: bool = False):
     log.info("Obteniendo resumen mensual...")
-    status_operation =SummaryReportBBIP().summary_monthly()
+    status_operation = SummaryReportBBIP().summary_monthly(literal=literal)
     if not status_operation: log.error("No se pudo generar el resumen mensual")
     else: log.info("Resumen mensual generado con éxito")
 
