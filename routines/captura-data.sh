8a01200bb37f0a5f2104cfac02b2a32feef8769e
xanv754 <angyeemarin.dev@gmail.com>
2025-07-03 15:33:16 -0400

    chore: delete unnecessary file

--- routines/captura-data.sh
+++ routines/captura-data.sh
@@ -21,7 +21,6 @@ ls $ruta/sources/SCAN > $ruta/routines/tmp/lista
 cat $ruta/routines/tmp/lista | while read line1
 do
   capa=`echo $line1 | sed 's/.txt//g'`
-  echo $capa > $ruta/routines/capa
   cat $ruta/sources/SCAN/$capa.txt | while read line2
   do
     url=`echo $line2 | awk '{print $1}' `
@@ -32,8 +31,8 @@ do
 
     wget --user=$USERSCAN --password=$PASSWORDSCAN --no-check-certificate $url
 
-    sed -i '1d' $ruta/routines/$terminal
-    cat $ruta/routines/$terminal | head -500 | while read line3
+    sed -i '1d' $ruta/routines/tmp/$terminal
+    cat $ruta/routines/tmp/$terminal | head -500 | while read line3
     do
       # Fecha y hora en formato UNIX
       fechaunix=`echo $line3 | awk '{print $1}'` 
@@ -57,11 +56,9 @@ do
     lineas=`cat $ruta/data/SCAN/$capa/$tipo\%$interfaz\%$capacidad | grep -f $ruta/routines/tmp/fechaayer | wc -l`
     hora=$(date +"%y-%m-%d %T")
     echo $hora $capa $interfaz $lineas >> $ruta/data/SCAN/Alertas.txt 
-    rm $ruta/routines/$terminal
+    rm $ruta/routines/tmp/$terminal
   done  
 done
 
-rm $ruta/routines/tmp/fechaayer
-rm $ruta/routines/tmp/lista
-rm $ruta/routines/tmp/capa
+rm $ruta/routines/tmp/*
 
