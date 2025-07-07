aa9de8bb26b6b08742305706acf4be7df088d8cd
xanv754 <angyeemarin.dev@gmail.com>
2025-07-07 08:44:00 -0400

    fix: calculate to reports

--- utils/calculate.py
+++ utils/calculate.py
@@ -3,19 +3,32 @@ from constants import HeaderBBIP, HeaderDailyReport, header_daily_report
 from utils.log import log
 
 
-def calculate(df: pd.DataFrame, factor: int = 0.000000008022) -> pd.DataFrame:
-    """Calculate the in and out prom and max prom with the use of a traffic dataframe."""
+def calculate(df: pd.DataFrame) -> pd.DataFrame:
+    """Calculate the in and out prom and max prom with the use of a traffic dataframe.
+    
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Traffic dataframe.
+
+    Returns
+    -------
+    pd.DataFrame
+        Summary dataframe.
+    """
     try:
         summary_df = pd.DataFrame(columns=header_daily_report)
         interfaces = df[HeaderBBIP.NAME].unique()
         for interface in interfaces:
-            df_filtered = df[df[HeaderBBIP.NAME] == interface]
+            df_filtered: pd.DataFrame = df[df[HeaderBBIP.NAME] == interface]
+            df_filtered = df_filtered.sort_values(by=[HeaderDailyReport.DATE], ascending=False)
+            df_filtered = df_filtered.reset_index(drop=True)
             capacity = df_filtered[HeaderBBIP.CAPACITY].iloc[0]
             type = df_filtered[HeaderBBIP.TYPE].iloc[0]
-            in_prom = (df_filtered[HeaderDailyReport.IN_PROM].mean()) * factor
-            out_prom = (df_filtered[HeaderDailyReport.OUT_PROM].mean()) * factor
-            in_max_prom = (float(df_filtered[HeaderDailyReport.IN_MAX].mean())) * factor
-            out_max_prom = (float(df_filtered[HeaderDailyReport.OUT_MAX].mean())) * factor
+            in_prom = (df_filtered[HeaderDailyReport.IN_PROM].mean())
+            out_prom = (df_filtered[HeaderDailyReport.OUT_PROM].mean())
+            in_max_prom = (float(df_filtered[HeaderDailyReport.IN_MAX].mean()))
+            out_max_prom = (float(df_filtered[HeaderDailyReport.OUT_MAX].mean()))
             try:
                 if in_max_prom >= out_max_prom:
                     use = (in_max_prom / capacity) * 100
