426629d4e5ba4e96737ae86df0ebc9e643c73398
xanv754 <angyeemarin.dev@gmail.com>
2026-05-20 14:19:35 -0400

    feat(database): add setup database MongoDB

--- scanbackup/infrastructure/persistence/mongodb/connections/database.py
+++ scanbackup/infrastructure/persistence/mongodb/connections/database.py
@@ -1,11 +1,17 @@
 from typing import Any, Dict
+from pathlib import Path
 from pymongo import MongoClient
+from pymongo.database import Database
 from pymongo.collection import Collection
 from shared import (
-    MongoConnectionError,
     MongoDatabaseError,
+    MongoConnectionError,
     MongoCreateCollectionError,
     MongoDeleteCollectionError,
+    MongoImportCollectionError,
+    MongoExportCollectionError,
+    DatabaseDataNotFoundError,
+    DatabaseDataContentError,
 )
 from scanbackup.infrastructure.persistence.mongodb.constants.collection import (
     MongoCollectionName,
@@ -154,12 +160,229 @@ class DatabaseMongo:
                 error=error, message="Error inesperado al crear colecciones"
             )
 
-    def drop(self) -> None:
+    def import_data(
+        self,
+        name_collection: MongoCollectionName,
+        filepath: str,
+        delimiter: str | None = None,
+    ) -> None:
+        filepath = Path(filepath)
+        try:
+            if not filepath.exists():
+                raise DatabaseDataNotFoundError()
+            if name_collection is BBIPCollection.LAYERS_VALID:
+                BBIPCollection.import_data(
+                    name_collection=name_collection,
+                    database=self._client,
+                    input_path=filepath,
+                    delimiter=delimiter,
+                )
+            elif name_collection is IPCollection.LAYERS_VALID:
+                IPCollection.import_data(
+                    name_collection=name_collection,
+                    database=self._client,
+                    input_path=filepath,
+                    delimiter=delimiter,
+                )
+            elif name_collection == MongoCollectionName.BBIP_SOURCES:
+                BBIPSourceCollection.import_data(
+                    database=self._client,
+                    input_path=filepath,
+                    delimiter=delimiter,
+                )
+            elif name_collection == MongoCollectionName.IP_SOURCES:
+                IPSourceCollection.import_data(
+                    database=self._client,
+                    input_path=filepath,
+                    delimiter=delimiter,
+                )
+            elif name_collection == MongoCollectionName.BBIP_DAILY_SUMMARY:
+                BBIPDailySummaryCollection.import_data(
+                    database=self._client,
+                    input_path=filepath,
+                    delimiter=delimiter,
+                )
+            elif name_collection == MongoCollectionName.IP_DAILY_SUMMARY:
+                IPDailySummaryCollection.import_data(
+                    database=self._client,
+                    input_path=filepath,
+                    delimiter=delimiter,
+                )
+            else:
+                raise ValueError("Nombre de colección inválido")
+        except MongoImportCollectionError:
+            raise
+        except DatabaseDataContentError:
+            raise
+        except DatabaseDataNotFoundError:
+            raise
+        except Exception as error:
+            raise MongoDatabaseError(
+                error=error, message="Error inesperado al importar la data"
+            )
+
+    def export_data(
+        self,
+        filepath: str | None = None,
+        name_collection: MongoCollectionName | None = None,
+        delimiter: str | None = None,
+        include_id: bool = True,
+    ) -> None:
+        if not filepath:
+            filepath = Path.home()
+        else:
+            filepath = Path(filepath)
+        try:
+            if not name_collection or name_collection == MongoCollectionName.BORDE:
+                BBIPCollection.export_data(
+                    name_collection=MongoCollectionName.BORDE,
+                    database=self._client,
+                    output_path=Path(filepath / f"{MongoCollectionName.BORDE}.csv"),
+                    delimiter=delimiter,
+                    include_id=include_id,
+                )
+            if not name_collection or name_collection == MongoCollectionName.BRAS:
+                BBIPCollection.export_data(
+                    name_collection=MongoCollectionName.BRAS,
+                    database=self._client,
+                    output_path=Path(filepath / f"{MongoCollectionName.BRAS}.csv"),
+                    delimiter=delimiter,
+                    include_id=include_id,
+                )
+            if not name_collection or name_collection == MongoCollectionName.CACHING:
+                BBIPCollection.export_data(
+                    name_collection=MongoCollectionName.CACHING,
+                    database=self._client,
+                    output_path=Path(filepath / f"{MongoCollectionName.CACHING}.csv"),
+                    delimiter=delimiter,
+                    include_id=include_id,
+                )
+            if not name_collection or name_collection == MongoCollectionName.RAI:
+                BBIPCollection.export_data(
+                    name_collection=MongoCollectionName.RAI,
+                    database=self._client,
+                    output_path=Path(filepath / f"{MongoCollectionName.RAI}.csv"),
+                    delimiter=delimiter,
+                    include_id=include_id,
+                )
+            if not name_collection or name_collection == MongoCollectionName.DINT:
+                BBIPCollection.export_data(
+                    name_collection=MongoCollectionName.DINT,
+                    database=self._client,
+                    output_path=Path(filepath / f"{MongoCollectionName.DINT}.csv"),
+                    delimiter=delimiter,
+                    include_id=include_id,
+                )
+            if not name_collection or name_collection == MongoCollectionName.DIST:
+                BBIPCollection.export_data(
+                    name_collection=MongoCollectionName.DIST,
+                    database=self._client,
+                    output_path=Path(filepath / f"{MongoCollectionName.DIST}.csv"),
+                    delimiter=delimiter,
+                    include_id=include_id,
+                )
+            if not name_collection or name_collection == MongoCollectionName.IXP:
+                BBIPCollection.export_data(
+                    name_collection=MongoCollectionName.IXP,
+                    database=self._client,
+                    output_path=Path(filepath / f"{MongoCollectionName.IXP}.csv"),
+                    delimiter=delimiter,
+                    include_id=include_id,
+                )
+            if not name_collection or name_collection == MongoCollectionName.IP_BRAS:
+                IPCollection.export_data(
+                    name_collection=MongoCollectionName.IP_BRAS,
+                    database=self._client,
+                    output_path=Path(filepath / f"{MongoCollectionName.IP_BRAS}.csv"),
+                    delimiter=delimiter,
+                    include_id=include_id,
+                )
+            if (
+                not name_collection
+                or name_collection == MongoCollectionName.BBIP_SOURCES
+            ):
+                BBIPSourceCollection.export_data(
+                    database=self._client,
+                    output_path=Path(
+                        filepath / f"{MongoCollectionName.BBIP_SOURCES}.csv"
+                    ),
+                    delimiter=delimiter,
+                    include_id=include_id,
+                )
+            if not name_collection or name_collection == MongoCollectionName.IP_SOURCES:
+                IPSourceCollection.export_data(
+                    database=self._client,
+                    output_path=Path(
+                        filepath / f"{MongoCollectionName.IP_SOURCES}.csv"
+                    ),
+                    delimiter=delimiter,
+                    include_id=include_id,
+                )
+            if (
+                not name_collection
+                or name_collection == MongoCollectionName.BBIP_DAILY_SUMMARY
+            ):
+                BBIPDailySummaryCollection.export_data(
+                    database=self._client,
+                    output_path=Path(
+                        filepath / f"{MongoCollectionName.BBIP_DAILY_SUMMARY}.csv"
+                    ),
+                    delimiter=delimiter,
+                    include_id=include_id,
+                )
+            if (
+                not name_collection
+                or name_collection == MongoCollectionName.IP_DAILY_SUMMARY
+            ):
+                IPDailySummaryCollection.export_data(
+                    database=self._client,
+                    output_path=Path(
+                        filepath / f"{MongoCollectionName.IP_DAILY_SUMMARY}.csv"
+                    ),
+                    delimiter=delimiter,
+                    include_id=include_id,
+                )
+            else:
+                raise ValueError("Nombre de colección inválido")
+        except MongoExportCollectionError:
+            raise
+        except ValueError:
+            if name_collection:
+                raise MongoExportCollectionError(
+                    name=name_collection, extra_msg="Nombre de la colección desconocido"
+                )
+            else:
+                raise
+        except Exception as error:
+            raise MongoDatabaseError(
+                error=error, message="Error inesperado al importar la data"
+            )
+
+    def drop(self, force: bool = False) -> None:
         """Deletes all collections in the database."""
         try:
             self.open_connection(self._uri)
             db = self._client[self._name_db]
+
+            if not force:
+                self.export_data()
+
+            BBIPCollection.delete(MongoCollectionName.BORDE, db)
+            BBIPCollection.delete(MongoCollectionName.BRAS, db)
+            BBIPCollection.delete(MongoCollectionName.CACHING, db)
+            BBIPCollection.delete(MongoCollectionName.RAI, db)
+            BBIPCollection.delete(MongoCollectionName.DINT, db)
+            BBIPCollection.delete(MongoCollectionName.DIST, db)
+            BBIPCollection.delete(MongoCollectionName.IXP, db)
+            BBIPDailySummaryCollection.delete(db)
+            BBIPSourceCollection.delete(db)
+
+            IPCollection.delete(MongoCollectionName.IP_BRAS, db)
+            IPDailySummaryCollection.delete(db)
+            IPSourceCollection.delete(db)
             self.close_connection()
+        except MongoExportCollectionError:
+            raise
         except MongoDeleteCollectionError:
             raise
         except MongoConnectionError:
