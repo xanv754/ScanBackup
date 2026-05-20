106ffce970026f6d310c18217157d88323b21fa9
xanv754 <angyeemarin.dev@gmail.com>
2026-05-20 14:20:11 -0400

    refactor(database): delete unnecessary import

--- scanbackup/infrastructure/persistence/mongodb/connections/database.py
+++ scanbackup/infrastructure/persistence/mongodb/connections/database.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict
 from pathlib import Path
 from pymongo import MongoClient
-from pymongo.database import Database
 from pymongo.collection import Collection
 from shared import (
     MongoDatabaseError,
