01fadf75dff5769a10a65621ee510cb32a562d8d
xanv754 <angyeemarin.dev@gmail.com>
2026-05-22 08:17:46 -0400

    feat(database): add import command to CLI

--- scanbackup/infrastructure/persistence/mongodb/cli.py
+++ scanbackup/infrastructure/persistence/mongodb/cli.py
@@ -2,6 +2,9 @@ import click
 from scanbackup.infrastructure.persistence.mongodb.connections.database import (
     MongoDatabase,
 )
+from scanbackup.infrastructure.persistence.mongodb.constants.collection import (
+    MongoCollectionName,
+)
 from scanbackup.shared import URIEnvironment, Terminal, Log
 
 
@@ -27,7 +30,7 @@ def setup(dev: bool = False):
             config = URIEnvironment(dev=dev)
             uri_mongo = config.get_uri_db()
 
-            terminal.loading(status, "Creando colecciones...")
+            terminal.loading(status, "Iniciando proceso...")
 
             mongo_database = MongoDatabase(uri=uri_mongo)
             mongo_database.initialize()
@@ -61,12 +64,59 @@ def drop(force: bool = False, dev: bool = False):
             config = URIEnvironment(dev=dev)
             uri_mongo = config.get_uri_db()
 
-            terminal.loading(status, "Eliminando colecciones...")
+            terminal.loading(status, "Iniciando proceso...")
 
             mongo_database = MongoDatabase(uri=uri_mongo)
             mongo_database.drop(force)
         except Exception:
             terminal.error("Eliminación de la base de datos fallida")
+            exit(1)
+        else:
+            terminal.info("Proceso finalizado con éxito")
+
+
+@cli.command("import", help="Importa datos de un archivo .csv a una colección")
+@click.option(
+    "--file",
+    type=click.Path(exists=True),
+    required=True,
+    help="Archivo .csv a importar",
+)
+@click.option(
+    "--collection",
+    type=click.Choice(MongoCollectionName),
+    required=True,
+    help="Colección a importar",
+)
+@click.option("--delimiter", help="Delimitador de campos")
+@click.option(
+    "--dev", is_flag=True, help="Carga las variables del entorno de desarrollo"
+)
+def import_data(
+    file: str,
+    collection: MongoCollectionName,
+    delimiter: str | None = None,
+    dev: bool = False,
+) -> None:
+    terminal = Terminal()
+
+    start_info = f"Importación de datos a la colección {collection}"
+    Log.info(start_info)
+    terminal.info(start_info)
+
+    with terminal.status("Cargando configuración del sistema...") as status:
+        try:
+            config = URIEnvironment(dev=dev)
+            uri_mongo = config.get_uri_db()
+
+            terminal.loading(status, "Iniciando proceso...")
+
+            mongo_database = MongoDatabase(uri=uri_mongo)
+            mongo_database.import_data(
+                name_collection=collection, filepath=file, delimiter=delimiter
+            )
+        except Exception:
+            terminal.error("Importación de datos fallida")
             exit(1)
         else:
             terminal.info("Proceso finalizado con éxito")
